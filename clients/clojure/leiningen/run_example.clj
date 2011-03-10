fc848209222d2482133bbc97dd190882af9a0225
asmyczek <adam.smyczek@gmail.com>
2011-03-09 21:52:04 -0800

    Updating example runner.

--- clients/clojure/leiningen/run_example.clj
+++ clients/clojure/leiningen/run_example.clj
@@ -1,10 +1,20 @@
 (ns leiningen.run-example
   (:use [leiningen.compile :only (eval-in-project)]))
 
+(def examples-map
+  {"1" {:name "Simple producer/consumer example."   :namespace 'examples.simple}})
+   
 (defn run-example
   [project & args]
-  (eval-in-project project
-    `(do
-       (require 'kafka.example)
-       (kafka.example/run))))
+  (if args
+    (if-let [exp (examples-map (first args))]
+      (eval-in-project project
+        `(let [ns# (:namespace '~exp)]
+           (println "------------------------------------------------")
+           (println "Running:" (:name '~exp))
+           (println "------------------------------------------------")
+           (require ns#)
+           ((get (ns-publics ns#) (symbol "run")))))
+      (println (str "Invalid example no. Expecting {1.." (count examples-map) "}.")))
+    (println (str "Provide example no. {1.." (count examples-map) "}."))))
 
